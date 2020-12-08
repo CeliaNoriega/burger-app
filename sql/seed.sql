7a9be75f536080ee52e676c51cb1b5703b932040
CeliaNoriega <noriegacelia2124@gmail.com>
2020-12-07 18:52:01 -0800

    database created

---
+++ sql/seed.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burger (burgerName, devoured)
+VALUES ('Double Cheeseburger', false), 
+('Bacon Avocado Cheeseburger', false), 
+('Chicken Club Burger', false), 
+('Veggie Burger', false);
